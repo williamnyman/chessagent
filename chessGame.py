3763557e692cc6e80fc20f772a22eccec40ae177
william-nyman <32524070+williamnyman@users.noreply.github.com>
2024-07-09 18:33:09 -0700

    Update chessGame.py

--- chessGame.py
+++ chessGame.py
@@ -1,1 +1,30 @@
+class ChessGame:
+    def __init__(self):
+        self.gameboard = ChessBoard()
+        self.playerblack = Player('black')
+        self.playerwhite = Player('white')
+        self.ticker = 0
+        self.victor = None
 
+    def startGame(self):
+        while not self.checkVictory():
+            self.gameTurn()
+        print(f"{self.checkVictory().color} player wins!")
+        
+    def gameTurn(self):
+        self.gameboard.display()
+
+        if self.ticker % 2 == 0:
+            self.playerwhite.populate_pieces(self.gameboard)
+            selected_piece, chosen_move = self.playerwhite.chooseMove(self.gameboard)
+            self.gameboard.applyMove(chosen_move, self.playerwhite, selected_piece)
+
+        else:
+            self.playerblack.populate_pieces(self.gameboard)
+            selected_piece, chosen_move = self.playerblack.chooseMove(self.gameboard)
+            self.gameboard.applyMove(chosen_move, self.playerblack, selected_piece)
+
+        self.ticker += 1
+
+    def checkVictory(self):
+        return self.gameboard.boardVictory(self.playerwhite, self.playerblack)
